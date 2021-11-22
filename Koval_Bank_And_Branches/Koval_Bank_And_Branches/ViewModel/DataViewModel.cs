9242c8dc0a37fa0eea8151764367083e26759b1f
Bea-tyxz <pkoval011@gmail.com>
2021-11-22 13:52:17 +0200

    Added functionality

--- Koval_Bank_And_Branches/Koval_Bank_And_Branches/ViewModel/DataViewModel.cs
+++ Koval_Bank_And_Branches/Koval_Bank_And_Branches/ViewModel/DataViewModel.cs
@@ -14,6 +14,18 @@ namespace Koval_Bank_And_Branches.ViewModel
     {
         private string _visibleControl = "User";
 
+        public ViewModelBase SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChange("SelectedItem");
+            }
+        }
+
+        private ViewModelBase _selectedItem;
+
         public string VisibleControl
         {
             get => _visibleControl;
@@ -27,10 +39,42 @@ namespace Koval_Bank_And_Branches.ViewModel
         public DataViewModel()
         {
             SetControlVisibility = new Command(ControlVisibility);
+            BindDeleteItem = new Command(DeleteItem);
         }
 
         public ICommand SetControlVisibility { get; set; }
+        public ICommand BindDeleteItem { get; set; }
 
+        public void DeleteItem(object args)
+        {
+            try
+            {
+                if (_visibleControl == "Contract" && _selectedItem != null)
+                {
+                    Contracts.Remove(Contracts.Where(item => item.Id == (_selectedItem as ContractViewModel).Id).FirstOrDefault());
+                }
+                else if (_visibleControl == "User" && _selectedItem != null)
+                {
+                    Users.Remove(Users.Where(item => item.Id == (_selectedItem as PersonViewModel).Id).FirstOrDefault());
+                }
+                else
+                {
+                    string messageBoxText = "Select item to delete it!";
+                    string caption = "Error";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Error;
+                    MessageBox.Show(messageBoxText, caption, button, icon);
+                }
+            }
+            catch (Exception exc)
+            {
+                string messageBoxText = exc.Message;
+                string caption = "Error";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+        }
         public void ControlVisibility(object args)
         {
             VisibleControl = args.ToString();
